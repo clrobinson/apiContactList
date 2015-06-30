fc1de9c948ff07dab1c6514bcdcce3c67de6dd82
Chris Robinson <clrobinson@users.noreply.github.com>
2015-06-30 19:53:32 +0000

    DB in place.

---
+++ db/migrate/1_make_contact_list_table.rb
@@ -0,0 +1,8 @@
+class MakeContactListTable < ActiveRecord::Migration
+  create_table :contacts do |t|
+    t.string :first_name
+    t.string :last_name
+    t.string :email
+    t.timestamps
+  end
+end
